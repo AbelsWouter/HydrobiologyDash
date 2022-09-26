3280471ad1f0d317a3cd5ed8c7d6c72450d586a5
AbelsWouter <wouter.abels@rws.nl>
2022-09-26 17:26:13 +0200

    re add files

---
+++ classes/LoadData.py
@@ -0,0 +1,71 @@
+import json
+import numpy as np
+import pandas as pd
+from dash import html
+
+class LoadData:
+
+    def footer_logo(app):
+        footer = 'Wouter Abels (wouterabels@rws.nl) 22 Augustus 2022 Python 3.10.6'
+        logo = ('IW_RW_Logo_online_pos_nl.png')
+        footer_logo = html.Div(
+            children=[
+                html.Footer(
+                    footer
+                ),
+                html.Img(
+                    id='logo',
+                    src=app.get_asset_url(logo)
+                )
+            ]
+        )
+        return footer_logo
+
+    def geo_data():
+        with open ('data/measurementobjects_rws.json', 'r') as f:
+            geo_data = json.load(f)
+        return geo_data
+    
+    def macev_data():
+        all_data = pd.read_csv(
+            'data/macev_2015_2021.csv',
+            dtype={'externalreference': str})
+        all_data = all_data.drop(
+            [
+                'name',
+                'taxongroup_twn',
+                'synonymname',
+                'maintypecode',
+                'code'
+            ],
+            axis= 1
+        )
+        current_data = all_data.loc[all_data['measurementdate'] >= '2021']
+        historic_data = all_data.loc[all_data['measurementdate'] <= '2021']
+        unique_measurementobject = np.sort(
+            pd.unique(
+                current_data['measurementobjectname']
+            )
+        )
+        historic_location= []
+        historic_and_current = []
+        for object in unique_measurementobject:
+            historic_per_location = historic_data.loc[historic_data['measurementobjectname'] == object]
+            all_per_location = all_data.loc[all_data['measurementobjectname'] == object]
+            historic_and_current = pd.concat(
+                [
+                    all_per_location,
+                    pd.DataFrame(
+                        historic_and_current
+                    )
+                ]
+            )
+            historic_location = pd.concat(
+                [
+                    historic_per_location,
+                    pd.DataFrame(
+                        historic_location
+                    )
+                ]
+            )
+        return current_data, historic_data, unique_measurementobject, historic_and_current
---
+++ pages/ValidationPage.py
@@ -0,0 +1,173 @@
+from classes.Validation import Validations
+from dash import dcc, html, dash_table, Input, Output, callback
+from classes.LoadData import LoadData
+
+# Load data #
+# MACEV data from 2015 till 2021
+current_data, historic_data, unique_measurementobject, historic_and_current = LoadData.macev_data()
+
+# Data validation #
+# Configure the validation page
+validation_page = html.Div(
+    children=[
+        html.Div(
+            id='navbar',
+            children=[
+                html.Header(
+                    children=[
+                        dcc.Link(
+                            'Home',
+                            href='/', 
+                            className='link'
+                        ),
+                        dcc.Link(
+                            'Abundantie',
+                            href='/abundantie',
+                            className='link'
+                        ),
+                        dcc.Link(
+                            'Validatie', 
+                            href='/validatie', 
+                            className='link_active'
+                        ),
+                        dcc.Link(
+                            'Statistiek', 
+                            href='/statistiek', 
+                            className='link'
+                        )
+                    ]
+                )
+            ],
+        ),
+        html.Div(
+            id='page',
+            children=[
+                html.H1(
+                    'Macro evertebraten'
+                ),
+                html.H2(
+                    '2015-2021'
+                ),
+                html.H2(
+                    'Validatie'
+                ),
+                html.Div(
+                    id='dropdown_and_table',
+                    children=[
+                        dcc.Dropdown(
+                            id='table_dropdown',
+                            options=[
+                                {
+                                    'label':'Collectienummer Validatie',
+                                    'value':'collection',
+                                },
+                                {
+                                    'label':'Taxonstatus Validatie',
+                                    'value':'taxonstatus',
+                                },
+                                {
+                                    'label':'Oligochaeten Validatie',
+                                    'value':'oligochaeta',
+                                },
+                                {
+                                    'label':'Chironomiden Validatie',
+                                    'value':'chironomidae',
+                                },
+                                {
+                                    'label':'Taxongroep Validatie',
+                                    'value':'taxongroup',
+                                },
+                                {
+                                    'label':'Factor Validatie', 
+                                    'value':'factor'
+                                },
+                                {
+                                    'label':'Missende Taxa',
+                                    'value':'missing'
+                                },
+                                {
+                                    'label':'Nieuwe Taxa', 
+                                    'value':'new'
+                                },
+                            ],
+                            value='collection',
+                            clearable=False
+                        ),
+                        dcc.Loading(
+                            children=[
+                                dash_table.DataTable(     
+                                    id='table_object',
+                                    columns=[
+                                        {'name': i,'id': i} for i in historic_and_current
+                                    ],
+                                    sort_action='native',
+                                    filter_action='native',
+                                    css=[
+                                        {
+                                            'selector':'.previous-page,'
+                                            '.next-page, .first-page,'
+                                            '.last-page, .export, .show-hide',
+                                            'rule':'color: black;',
+                                        },
+                                        {
+                                            'selector':'.current-page',
+                                            'rule':'padding-right: 5px;',
+                                        },
+                                    ],
+                                    style_cell={
+                                        'whiteSpace':'normal',
+                                        'width':'100px',
+                                        'textAlign':'center',
+                                        'height':'15px',
+                                        'padding-left':'10px',
+                                        'padding-right':'10px',
+                                    },
+                                    style_data_conditional=[
+                                        {
+                                            'if': {'row_index':'odd'},
+                                            'backgroundColor':'rgb(240, 240, 240)',
+                                        },
+                                    ],
+                                    style_table={
+                                        'height':'auto',
+                                        'width':'auto',
+                                        'overflowX':'auto',
+                                        'overflowY':'auto',
+                                    },
+                                    cell_selectable=False,
+                                    page_size=16,
+                                    style_as_list_view=True,
+                                )
+                            ]
+                        )
+                    ]
+                )
+            ]
+        )
+    ]
+)
+
+# App callback for the dropdown menu in validation page
+@callback(
+    Output(
+        'table_object', 'data'
+    ),
+    Input(
+        'table_dropdown', 'value'
+    )
+)
+
+# The table selections with validation data
+def table_update(dropdown_value):
+    current_data_validations = [
+        'collection', 'taxonstatus', 'oligochaeta', 'chironomidae', 'taxongroup', 'factor'
+    ]
+    current_and_historic_data_validations = [
+        'missing', 'new'
+    ]
+    if dropdown_value in current_data_validations:
+        table_data = getattr(Validations, dropdown_value)
+        return table_data(current_data).to_dict('records')
+    elif dropdown_value in current_and_historic_data_validations:
+        table_data = getattr(Validations, dropdown_value)
+        return table_data(current_data, historic_data).to_dict('records')
